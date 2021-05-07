e0de996725848dad8c5d95379975a583c8421bdf
Jakob Reinwald <jakob.reinwald@yahoo.com>
2021-05-07 04:58:08 -0700

    Fixed sample code

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,5 +1,5 @@
 import * as api from '../api';
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
 export const getPosts = () => async (dispatch) => {
     try {
@@ -45,7 +45,7 @@ export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id);
 
-        dispatch({ type: UPDATE, payload: data });
+        dispatch({ type: LIKE, payload: data });
     } catch (error) {
         console.log(error);
     }
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
